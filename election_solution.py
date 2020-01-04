84d12e5c64d0eb6f9cde6d0b1ad3690c93338767
philip-morlier <58234998+philip-morlier@users.noreply.github.com>
2020-01-03 19:48:18 -0800

    added total_counts and khan_counts to code

---
+++ election_solution.py
@@ -0,0 +1,25 @@
+import csv
+import os
+
+pypoll_csv = os.path.join('election_data.csv')
+
+
+mylist = []
+with open(pypoll_csv, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    for line in csvreader:
+        mylist.append(line)
+
+def total_votes(columns):
+    sum = 0 
+    for line in mylist:
+        if 'Voter' not in line[0]:
+            sum = sum + 1
+    return sum
+
+def khan_votes(columns):
+    khan_sum = 0 
+    for line in mylist:
+        if 'Candidate' not in line[2] and 'Khan' in line[2]:
+            khan_sum = khan_sum + 1
+    return khan_sum
